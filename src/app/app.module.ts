b74e7dbd41e6226aad577f5e2c763e62a8962f89
gustavo estevam sena <gustavoestevamsena@gmail.com>
2021-04-05 10:00:48 -0300

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AutoresComponent } from './autores/autores.component' 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutoresComponent
   ],
   imports: [
     BrowserModule,
